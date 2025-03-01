3d4f4fd9de028287f5f3204d3982db07c80b3b4f
Kairixir <kairixir@protonmail.com>
2025-03-01 23:55:14 +0100

    feat: add setup for vms and copy connection files

---
+++ proxmox/k8s-cluster-01/variables.tf
@@ -0,0 +1,11 @@
+variable "proxmox_api_endpoint" {
+  type        = string
+  default     = null
+  description = "Proxmox API endpoint URL"
+}
+
+variable "proxmox_api_token" {
+  type        = string
+  default     = null
+  description = "Proxmox API access token"
+}
